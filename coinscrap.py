9a771c94117e9d34527121259c79793165754d63
atandon <91836420+anshika2104@users.noreply.github.com>
2024-06-07 12:29:50 +0530

    Add files via upload

---
+++ coinscrap.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+driver.maximize_window()
+driver.get("https://coinmarketcap.com/currencies/duko")
